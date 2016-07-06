ec373c6bdc9b364868f3368362859ea57446107d
Antonin Stefanutti <antonin@stefanutti.fr>
2016-07-06 12:00:54 +0200

    Custom directive to improve templates loading from cache

--- plugins/profiles/ts/profilesPlugin.ts
+++ plugins/profiles/ts/profilesPlugin.ts
@@ -13,7 +13,7 @@ module Profiles {
 
   export var module = angular.module(pluginName, ['hawtio-core', 'hawtio-ui', 'hawtio-forms']);
 
-  module.config(["$routeProvider", ($routeProvider:ng.route.IRouteProvider) => {
+  module.config(['$routeProvider', ($routeProvider:ng.route.IRouteProvider) => {
     var join = UrlHelpers.join;
     var route = Developer.createTabRoutingFunction(templatePath);
     var base = '/workspaces/:namespace/projects/:projectId/profiles';
@@ -24,21 +24,39 @@ module Profiles {
         Forge.updateForgeProject(context.$scope);
       }
       return [{
-        isValid: () => context.projectLink && Developer.forgeReadyLink() && Forge.forgeProject().hasPerspective("fabric8-profiles"),
-        href: join(context.projectLink, "profiles"),
-        class: "fa fa-code-fork",
-        label: "Profiles",
-        title: "Browse the profiles of this project",
+        isValid: () => context.projectLink && Developer.forgeReadyLink() && Forge.forgeProject().hasPerspective('fabric8-profiles'),
+        href: join(context.projectLink, 'profiles'),
+        class: 'fa fa-code-fork',
+        label: 'Profiles',
+        title: 'Browse the profiles of this project',
         isActive: (subTab, path) => _.startsWith(path, join(context.projectLink, 'profiles'))
       }]
     });
 
     $routeProvider
-        .when(base, route('profiles.html', false))
-        .when(join(base, 'containers', 'assignProfiles'), route('assignProfiles.html', false))
-        .when(join(base, 'containers'), route('containers.html', false))
-        .when(join(base, 'settings'), route('settings.html', false));
+      .when(base, route('profiles.html', false))
+      .when(join(base, 'containers', 'assignProfiles'), route('assignProfiles.html', false))
+      .when(join(base, 'containers'), route('containers.html', false))
+      .when(join(base, 'settings'), route('settings.html', false));
   }]);
+
+  // Directive that avoids rendering glitches when using ng-include and bypasses its complexity
+  //as we use gulp-angular-templatecache to register AngularJS templates in the $templateCache.
+  module.directive('templateCacheInclude', ['$compile', '$templateCache', ($compile, $templateCache) => {
+    return {
+      restrict: 'A',
+      priority: 400,
+      compile: function (element, attrs) {
+        var templateName = attrs.templateCacheInclude;
+        var template = $templateCache.get(templateName);
+        return function (scope, element) {
+          element.html(template);
+          $compile(element.contents())(scope);
+        };
+      }
+    };
+  }
+  ]);
 
   hawtioPluginLoader.addModule(pluginName);
 }
